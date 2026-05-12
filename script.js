b28b5f06b2b2c3a7cb485d5cb536e9966197dbc8
Josue Alexander Vasquez Portillo <josuealexandervasquezportillo@gmail.com>
2026-05-12 13:37:34 -0600

    Mejoras to index.css
    Improve visual aestetich for better UI in the project avoid fill the display whit garbage

---
+++ script.js
@@ -0,0 +1,7 @@
+// ¿Dónde guardo los datos?: Si generas un número, ¿cómo evitas que salga dos veces? Necesitarás algo para recordar qué números ya salieron.
+
+// El momento del "clic": Tienes 4 botones principales. Cada uno necesita un "oído" (addEventListener). ¿Qué debería pasar exactamente cuando presionas "Generar"?
+
+// La conexión: ¿Cómo sacas el valor que la persona escribió en el input id="caja"?
+
+// El movimiento de datos: Si el número está en la pantalla principal y presionas "Confirmar", ¿qué debe pasar con ese número y a qué lista (id="lista-ganadores") debe viajar?
