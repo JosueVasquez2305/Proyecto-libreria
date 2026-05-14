01b027c3846a3a902c283bbbf9c1ba457a516240
Josue Alexander Vasquez Portillo <josuealexandervasquezportillo@gmail.com>
2026-05-14 14:20:18 -0600

    feat(Add js functions) I added the functions for js randomizer number and conect the  button with the event

--- script.js
+++ script.js
@@ -6,4 +6,18 @@
 
  // El movimiento de datos: Si el número está en la pantalla principal y presionas "Confirmar", ¿qué debe pasar con ese número y a qué lista (id="lista-ganadores") debe viajar?
 
-const participantes = document.getElementById["caja"].value.
+
+
+// 1. Conexión con el botón
+const botonSortear = document.getElementById("btn-generar"); 
+
+botonSortear.addEventListener("click", function() {
+    // 2. Captura y conversión a número
+    const totalParticipantes = Number(document.getElementById("caja").value);
+
+    // 3. La fórmula del azar (del 1 al total)
+    const numeroGanador = Math.floor(Math.random() * totalParticipantes) + 1;
+
+    // 4. El "reemplazo" en pantalla (adiós al '?' y bienvenido el ganador)
+    document.getElementById("numero-sorteado").textContent = numeroGanador;
+});
